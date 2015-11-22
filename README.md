457efac4ce9081d142c12d74febdeb6941d184fa
Hakan Bilgin <hbi99@hotmail.com>
2015-11-22 10:11:30 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+A QureFS wrapper for filesystem API.
+
+#### Usage
+
+#### Install
+
+```bash
+$ npm install qure-fs --save
+```
+
+#### Example
+
+## LICENSE
+
+(MIT License)
+
+See the LICENSE file for details
---
+++ index.js
@@ -0,0 +1,34 @@
+
+'use strict';
+
+module.exports = {
+	open: function() {
+		this.mysql = require('mysql');
+		this.conn = this.mysql.createConnection({
+			host     : 'localhost',
+			user     : 'me',
+			password : 'secret',
+			database : 'my_db'
+		});
+	},
+	select: function(query) {
+		var that = this;
+
+		// pause the queue
+		this.pause();
+
+		// open database connection
+		this.open();
+
+		// execute query
+		this.conn.query(query, function(err, rows, fields) {
+			if (err) throw err;
+
+			// close database connection
+			that.conn.end();
+
+			// resume queue
+			that.resume(rows);
+		});
+	}
+};
